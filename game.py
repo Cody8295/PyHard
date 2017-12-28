fe60b9bee4cdb1a40c861a9be1f69b8af2ccb0e4
Cody DallaValle <codydallavall2@gmail.com>
2017-12-28 19:34:36 +0000

    basic game loop implemented

---
+++ game.py
@@ -0,0 +1,26 @@
+# Cody DallaValle
+# PyHard
+
+import sys, pygame
+from pygame.locals import *
+
+pygame.init()
+clock = pygame.time.Clock()
+hndl = pygame.display.set_mode((300, 600))
+pygame.display.set_caption("PyHard")
+
+red = pygame.Color(255, 0, 0)
+green = pygame.Color(0, 255, 0)
+blue = pygame.Color(0, 0, 255)
+black = pygame.Color(0, 0, 0)
+white = pygame.Color(255, 255, 255)
+
+alive = True
+plyPos = (0, 0)
+
+while True:
+    if(alive)
+        hndl.fill(white)
+    else
+        hndl.fill(black)
+    
