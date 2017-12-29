ba0bdfc422d7ad27cab56f2e31415d39b63a0e2a
Cody DallaValle <codydallavall2@gmail.com>
2017-12-29 02:01:36 +0000

    main menu implemented

--- game.py
+++ game.py
@@ -4,9 +4,12 @@
 import sys, pygame
 from pygame.locals import *
 
+def generateMap():
+    print "generating"
+
 pygame.init()
 clock = pygame.time.Clock()
-hndl = pygame.display.set_mode((300, 600))
+hndl = pygame.display.set_mode((500, 300))
 pygame.display.set_caption("PyHard")
 
 red = pygame.Color(255, 0, 0)
@@ -15,12 +18,36 @@ blue = pygame.Color(0, 0, 255)
 black = pygame.Color(0, 0, 0)
 white = pygame.Color(255, 255, 255)
 
+mainMenuFont = pygame.font.SysFont("monospace", 36)
+mainMenuTxt = mainMenuFont.render("PyHard", 1, red)
+
+font1 = pygame.font.SysFont("monospace", 18)
+mainMenuStart = font1.render("Start", 1, black)
+startBounds = pygame.Rect(250, 90, 100, 25)
+
+starting = True;
 alive = True
 plyPos = (0, 0)
 
 while True:
-    if(alive)
-        hndl.fill(white)
-    else
-        hndl.fill(black)
     
+    if(alive):
+	hndl.fill(white)
+	if(starting):
+	    global startButton
+	    hndl.blit(mainMenuTxt, (220,50))
+	    
+
+	    startButton = pygame.draw.rect(hndl, green, startBounds)
+	    hndl.blit(mainMenuStart, (275, 90))
+    else:
+	hndl.fill(black)
+    for event in pygame.event.get():
+	if event.type == pygame.MOUSEBUTTONDOWN and event.button==1:
+	    
+	    pos = pygame.mouse.get_pos()
+	    if startButton.collidepoint(pos):
+		print "Game start"
+
+    pygame.display.update()
+    clock.tick(30)
